52806de8e1ddd9125bca24cea1a4b2558275e9d3
Danilo Santos <danilofatec27@gmail.com>
2026-03-25 09:20:52 -0300

    teste sem problemas

--- src/soma.js
+++ src/soma.js
@@ -1,12 +1,5 @@
-// function soma(a, b) {
-//  let resultado = a + b;
-//  let x = 10;
-//  return resultado;
-// }
-
 function soma(a, b) {
  let resultado = a + b;
-//  let x = 10;
  return resultado;
 }
 
